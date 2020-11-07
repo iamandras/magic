bfe6ac04ef698bb15372e48c099266d837180a62
Andras Szabo <andras@org50.com>
2020-11-07 11:44:26 +0100

    router loading fix

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -11,7 +11,7 @@ class Router
 
     public function __construct()
     {
-        include BASE_PATH . '/config/routes.php';
+        include constant('BASE_PATH') . '/config/routes.php';
 
         $this->routes = getRoutes();
     }
