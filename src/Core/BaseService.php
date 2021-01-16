d8bc792393f98c7dd19c98bd8233d7f7829657b5
Andras Szabo <andras@org50.com>
2021-01-16 22:49:49 +0100

    version number + base service changes

--- src/Core/BaseService.php
+++ src/Core/BaseService.php
@@ -5,16 +5,35 @@ declare(strict_types=1);
 namespace MagicFramework\Core;
 
 use Exception;
+use MagicFramework\Core\Database\PDOLayer;
 
 class BaseService
 {
+    protected PDOLayer $dbLayer;
+
+    public function __construct(PDOLayer $dbLayer)
+    {
+        $this->dbLayer = $dbLayer;
+    }
+
     /**
      * @throws ApiException
      */
-    protected function handleException(Exception $e): void
+    public function handleException(Exception $e): void
     {
+        $this->dbLayer->rollback();
         $apiException = new ApiException(ApiException::INTERNAL_ERROR);
         $apiException->setException($e);
         throw $apiException;
+    }
+
+    public function beginTransaction(): void
+    {
+        $this->dbLayer->beginTransaction();
+    }
+
+    public function commit(): void
+    {
+        $this->dbLayer->commit();
     }
 }
