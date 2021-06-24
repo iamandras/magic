a8b1baeb47077a74349046102fdf2648e0da8812
iamandras <szabo.andras4@gmail.com>
2021-06-24 12:34:35 +0200

    cors fix

--- src/Core/RequestHandler.php
+++ src/Core/RequestHandler.php
@@ -6,14 +6,13 @@ namespace MagicFramework\Core;
 
 class RequestHandler
 {
-    private function cors()
+    private function cors(): void
     {
+        header("Access-Control-Allow-Origin: *");
 
-        // Allow from any origin
         if (isset($_SERVER['HTTP_ORIGIN'])) {
             // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
             // you want to allow, and if so:
-            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
             header('Access-Control-Allow-Credentials: true');
             header('Access-Control-Max-Age: 86400');    // cache for 1 day
         }
